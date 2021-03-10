1c6add3bd9cd6350c1bac433209935637bef1f39
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-10 10:40:36 -0500

    completed 'ageToAbilities' function. All tests Pass for function

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
 // ┌─────────────────┐
-// │ Your code here! │	
+// │ Your code here! │
 // └─────────────────┘
-
-
+function ageToAbilities(num) {
+	if (num < 16) {
+		return "You can't drive.";
+	}
+	if (num >= 16 && num <= 17) {
+		return "You can drive but not vote.";
+	}
+	if (num >= 18 && num <= 24) {
+		return "You can vote but not rent a car.";
+	} else {
+		return "You can do pretty much anything.";
+	}
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
 
-if (typeof ageToAbilities === 'undefined') {
-    ageToAbilities = undefined;
+if (typeof ageToAbilities === "undefined") {
+	ageToAbilities = undefined;
 }
-if (typeof oddIndices === 'undefined') {
-    oddIndices = undefined;
+if (typeof oddIndices === "undefined") {
+	oddIndices = undefined;
 }
-if (typeof numOddValues === 'undefined') {
-    numOddValues = undefined;
+if (typeof numOddValues === "undefined") {
+	numOddValues = undefined;
 }
-if (typeof averageStringLength === 'undefined') {
-    averageStringLength = undefined;
+if (typeof averageStringLength === "undefined") {
+	averageStringLength = undefined;
 }
-if (typeof firstPunctuationIndex === 'undefined') {
-    firstPunctuationIndex = undefined;
+if (typeof firstPunctuationIndex === "undefined") {
+	firstPunctuationIndex = undefined;
 }
-if (typeof getPlace === 'undefined') {
-    getPlace = undefined;
+if (typeof getPlace === "undefined") {
+	getPlace = undefined;
 }
 
 module.exports = {
-    ageToAbilities,
-    oddIndices,
-    numOddValues,
-    averageStringLength,
-    firstPunctuationIndex,
-    getPlace
+	ageToAbilities,
+	oddIndices,
+	numOddValues,
+	averageStringLength,
+	firstPunctuationIndex,
+	getPlace,
 };
